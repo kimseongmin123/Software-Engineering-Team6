5691b8abc2f87c4cd2a5f7b411652311a91c42a0
Seun9w00 <ph1losophe@naver.com>
2024-05-15 22:45:13 +0900

    chore: window 폴더 생성 및 파일 이동

--- src/window/data_managing.py
+++ src/window/data_managing.py
@@ -22,7 +22,7 @@ def update_word(words, word, meaning):
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("data/DataManage.ui")[0]
+form_class = uic.loadUiType("src/ui/DataManage.ui")[0]
 
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
