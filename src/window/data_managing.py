5f773d9990eff35442aba79f659162fe33702012
kimseongmin123 <63985334+kimseongmin123@users.noreply.github.com>
2024-05-28 12:06:46 +0900

    Update data_managing.py
    fix: 객체 이름 수정, 기타 버그 수정

--- src/window/data_managing.py
+++ src/window/data_managing.py
@@ -1,9 +1,26 @@
 import json
-from data_processing import *
 
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt6.QtWidgets import *
+from PyQt6 import uic
+
+sys.path.append('.')
+
+import pandas as pd
+from src.module.data_processing import *
+
+df = pd.read_csv('data/hackers_test/hackers_test_raw.csv', encoding='cp949')
+
+df.drop(['Day', 'Unnamed: 4', 'Unnamed: 5', '외움'], axis=1, inplace=True)
+
+df['뜻'] = df['뜻'].apply(remove_parentheses).apply(remove_comma).apply(remove_semicolon).apply(remove_whitespace)
+df = df[~df['뜻'].str.contains('~')]
+
+dict = {row[0]: row[1] for row in df.itertuples(index=False)}
+
+
+with open('data/hackers_test/hackers_test_processed.json', 'w', encoding='utf-8') as f:
+    f.write(dict_to_json(dict))
 
 def save_words(words, filename):
     with open(filename, 'w', encoding='utf-8') as file:
@@ -31,7 +48,6 @@ class WindowClass(QMainWindow, form_class) :
         self.setupUi(self)
         self.pushButton.clicked.connect(self.button_add)
         self.pushButton_2.clicked.connect(self.button_remove)
-        self.pushButton_3.clicked.connect(self.button_update)
     def button_add(self) :
         filename = 'data/hackers_test/hackers_test_processed.json'
         words = json_to_dict('hackers_test')
@@ -42,15 +58,15 @@ class WindowClass(QMainWindow, form_class) :
     def button_remove(self) :
         filename = 'data/hackers_test/hackers_test_processed.json'
         words = json_to_dict('hackers_test')
-        word = self.LineEdit.text()
-        meaning = self.LineEdit_2.text()
-        remove_word(words, word, meaning)
+        word = self.lineEdit.text()
+        meaning = self.lineEdit_2.text()
+        remove_word(words, word)
         save_words(words, filename)
     def button_update(self) :
         filename = 'data/hackers_test/hackers_test_processed.json'
         words = json_to_dict('hackers_test')
-        word = self.LineEdit.text()
-        meaning = self.LineEdit_2.text()
+        word = self.lineEdit.text()
+        meaning = self.lineEdit_2.text()
         update_word(words, word, meaning)
         save_words(words, filename)
 
@@ -66,5 +82,5 @@ if __name__ == "__main__" :
     myWindow.show()
 
     #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
-    app.exec_()
+    app.exec()
 
